3232d051ceb7f2f7b36eb0aeeb487b6d88c2b720
Ripper <vivekkumar.62120@gmail.com>
2021-02-27 22:22:58 +0530

    hello

---
+++ JS-Basics/datatypes.js
@@ -0,0 +1,13 @@
+function a (){
+    return 1;
+}
+
+console.log(1208397, typeof 1208397)
+console.log(7386.892 , typeof 7386.892)
+console.log('a', typeof 'a')
+console.log("fasifhihei", typeof "fasifhihei")
+console.log(true , typeof true)
+console.log(null , typeof null)
+console.log(undefined , typeof undefined)
+console.log(a ,typeof a)
+console.log(a() , typeof a())
