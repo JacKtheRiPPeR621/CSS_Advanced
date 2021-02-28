e9b329e1678b38ebdbd2bfaad241b0c1d778e885
Ripper <vivekkumar.62120@gmail.com>
2021-02-28 16:10:55 +0530

    hi

--- JS-Basics/functions.js
+++ JS-Basics/functions.js
@@ -7,8 +7,18 @@ function B(){
     return 'B'
 }
 
-console.log(gamma())   /* Reference Error  */
+// console.log(gamma())   /* Reference Error  */
 
 let gamma = function() {   /* Intialization  */
     return 'C'
 }
+
+function area( height , width){
+    if(width == undefined){    /* if(!width) */
+        return Math.PI * height * height
+    }
+    return height * width
+}
+
+console.log('area 3,4' , area(3,4))
+console.log('area 4' , area(4))
